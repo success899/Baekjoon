1824a38d859f4b7629b8a81cf4bf6c220d151352
success899 <success899@naver.com>
2022-12-13 06:32:15 +0900

    solved 25501

---
+++ python/25501.py
@@ -0,0 +1,24 @@
+import sys
+input=sys.stdin.readline
+
+def recursion(s, l, r):
+    global count
+    count += 1
+    
+    if l >= r:
+        return 1
+    elif s[l] != s[r]:
+        return 0
+    else:
+        return recursion(s, l+1, r-1)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1)
+
+
+T = int(input())
+
+for _ in range(T):
+    count = 0
+    S = str(input()).rstrip()
+    print(isPalindrome(S), count)
