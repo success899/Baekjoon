7e355550d9105e4504731767baf0cc165ce240c8
success899 <success899@naver.com>
2023-01-18 21:26:16 +0900

    solved 15655

---
+++ python/15655.py
@@ -0,0 +1,20 @@
+import sys
+input=sys.stdin.readline
+
+N, M = map(int,input().split())
+N_list = list(map(int,input().split()))
+N_list.sort()
+Num_tmp = []
+
+def func(start_index):
+    if len(Num_tmp) == M:
+        print(' '.join(map(str, Num_tmp)))
+        return None
+
+    for i in range(start_index, len(N_list)):
+        if N_list[i] not in Num_tmp:
+            Num_tmp.append(N_list[i])
+            func(i + 1)
+            del Num_tmp[-1]
+
+func(0)
