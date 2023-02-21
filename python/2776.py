6daa8e820a8444e734a233e6f2d9adf8e7d1b4e2
success899 <success899@naver.com>
2023-02-22 06:47:56 +0900

    solved 2776

---
+++ python/2776.py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    N_list = set(list(map(int, input().split())))
+    M = int(input())
+    M_list = list(map(int, input().split()))
+
+    for i in M_list:
+        if i in N_list:
+            print(1)
+        else:
+            print(0)
