6ef57592c2f28e7fbaa71f2cc9a54b1142d8e298
success899 <success899@naver.com>
2023-03-12 10:46:14 +0900

    solved 4158

---
+++ python/4158.py
@@ -0,0 +1,23 @@
+import sys
+from collections import defaultdict
+input=sys.stdin.readline
+
+while True:
+    tmp = list(map(int, input().split()))
+    result = 0
+    
+    if len(tmp) == 2:
+        if tmp[0] == 0 and tmp[1] == 0:
+            break
+        else:
+            N, M = tmp[0], tmp[1]
+            N_M_dict = defaultdict(int)
+    for _ in range(N):
+        num = int(input())
+        N_M_dict[num] = 1
+    for _ in range(M):
+        num = int(input())
+        if N_M_dict.get(num) == 1:
+            result += 1
+
+    print(result)
