5ee96880008f2072c7871cd3242f79a13b18934e
success899 <success899@naver.com>
2023-05-17 23:04:55 +0900

    solved 8611

--- python/8611.py
+++ python/8611.py
@@ -5,17 +5,17 @@ n = int(input())
 tmp = True
 
 for i in range(2,11):
-  m = n
-  result = ""
+    m = n
+    result = ""
 
-  while m > 0:
-    result = str(m % i) + result
-    m = m // i
+    while m > 0:
+        result = str(m % i) + result
+        m = m // i
 
-  if result == result[::-1]:
-    print(i, end=' ')
-    print(result)
-    tmp = False
+    if result == result[::-1]:
+        print(i, end=' ')
+        print(result)
+        tmp = False
 
 if tmp == True:
-  print("NIE")
+    print("NIE")
