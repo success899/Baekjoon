75547456890fd024b4107fdcaa91089cc9e09512
success899 <success899@naver.com>
2023-05-22 20:01:19 +0900

    solved 9372

---
+++ python/9372.py
@@ -0,0 +1,12 @@
+import sys
+input=sys.stdin.readline
+
+T = int(input())
+
+for i in range(T):
+    N, M = map(int, input().split())
+    
+    for j in range(M):
+        a, b = map(int, input().split())
+        
+    print(N - 1)
