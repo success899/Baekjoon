256b50ed33c85a9adb29724b50d49c97e53141f1
success899 <success899@naver.com>
2023-06-13 10:44:57 +0900

    solved 26099

---
+++ python/26099.py
@@ -0,0 +1,20 @@
+import sys
+input=sys.stdin.readline
+
+import sys
+input=sys.stdin.readline
+
+n = int(input())
+
+count = 0
+
+while n >= 0:
+    if n%5 == 0:
+        count += int(n//5)
+        print(count)
+        break
+    n -= 3
+    count += 1
+
+else:
+    print(-1)
