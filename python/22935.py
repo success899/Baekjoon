db055cf93be05135fb242dfd5668016430658b5c
success899 <success899@naver.com>
2023-08-05 01:22:02 +0900

    solved 22935

---
+++ python/22935.py
@@ -0,0 +1,15 @@
+import sys
+input=sys.stdin.readline
+
+num_list = [2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3]
+
+for k in range(int(input())):
+    num = int(input())
+    key = num % 28
+    change = "{0:b}".format(num_list[key]).zfill(4)
+    for v in change:
+        if v == '0':
+            print('V', end='')
+        else:
+            print('딸기', end='')
+    print("")
