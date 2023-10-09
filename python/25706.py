cd7af736427e12e1ed79c8d66cc43d4064974d12
success899 <success899@naver.com>
2023-10-09 19:34:57 +0900

    solved 25706

--- python/25706.py
+++ python/25706.py
@@ -6,7 +6,7 @@ N_list = list(map(int, input().split()))
 result = []
 result.append(1)
 
-for i in range (N-1,0,-1):
+for i in range (N-1, 0, -1):
     if N > i+N_list[i-1]:
         tmp = result[N - i - N_list[i-1] - 1]
         result.append(tmp + 1)
