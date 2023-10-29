75fc81ff3114dcc993b6a5d1654e42799220c48e
success899 <success899@naver.com>
2023-10-29 13:44:31 +0900

    solved 17175

---
+++ python/17175.py
@@ -0,0 +1,12 @@
+import sys
+input=sys.stdin.readline
+
+n = int(input())
+
+if n < 2:
+    print(1)
+else:
+    a, b = 1, 1
+    for i in range(n-1):
+        a, b = a + b + 1, a
+    print(a % 1000000007)
